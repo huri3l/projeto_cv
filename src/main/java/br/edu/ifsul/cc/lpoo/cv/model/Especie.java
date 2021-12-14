7bfd16d21c7382a55675d17b74f9cd99d668e150
Huriel <huri3l.lopes@gmail.com>
2021-12-14 20:34:03 -0300

    feat: Initial commit

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Especie.java
@@ -0,0 +1,14 @@
+package br.edu.ifsul.cc.lpoo.cv.model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author huriel
+ */
+public class Especie {
+    
+}
