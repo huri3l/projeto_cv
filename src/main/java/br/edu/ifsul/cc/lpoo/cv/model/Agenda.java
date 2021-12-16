a888c63507681f4b9cf8e32318185c8da8b5fa30
Huriel <huri3l.lopes@gmail.com>
2021-12-16 01:01:11 -0300

    feat: implements all classes attributes

--- src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Agenda.java
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Agenda.java
@@ -4,10 +4,157 @@
  */
 package br.edu.ifsul.cc.lpoo.cv.model;
 
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
  *
  * @author huriel
  */
-public class Agenda {
+@Entity
+@Table(name = "tb_agenda")
+public class Agenda implements Serializable {
     
+    @Id
+    @SequenceGenerator(name = "seq_agenda", sequenceName = "seq_agenda_id", allocationSize = 1)
+    @GeneratedValue(generator = "seq_agenda", strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar data_inicio;
+    
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar data_fim;
+    
+    @Column(nullable = true)
+    private String observacao;
+    
+    @ManyToOne
+    @JoinColumn(name = "funcionario_id", nullable = false)
+    private Funcionario funcionario;
+    
+    @ManyToOne
+    @JoinColumn(name = "medico_id", nullable = false)
+    private Medico medico;
+    
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TipoProduto tipo_produto;
+    
+    public Agenda() {
+        
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the data_inicio
+     */
+    public Calendar getData_inicio() {
+        return data_inicio;
+    }
+
+    /**
+     * @param data_inicio the data_inicio to set
+     */
+    public void setData_inicio(Calendar data_inicio) {
+        this.data_inicio = data_inicio;
+    }
+
+    /**
+     * @return the data_fim
+     */
+    public Calendar getData_fim() {
+        return data_fim;
+    }
+
+    /**
+     * @param data_fim the data_fim to set
+     */
+    public void setData_fim(Calendar data_fim) {
+        this.data_fim = data_fim;
+    }
+
+    /**
+     * @return the observacao
+     */
+    public String getObservacao() {
+        return observacao;
+    }
+
+    /**
+     * @param observacao the observacao to set
+     */
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    /**
+     * @return the funcionario
+     */
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    /**
+     * @param funcionario the funcionario to set
+     */
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    /**
+     * @return the medico
+     */
+    public Medico getMedico() {
+        return medico;
+    }
+
+    /**
+     * @param medico the medico to set
+     */
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    /**
+     * @return the tipo_produto
+     */
+    public TipoProduto getTipo_produto() {
+        return tipo_produto;
+    }
+
+    /**
+     * @param tipo_produto the tipo_produto to set
+     */
+    public void setTipo_produto(TipoProduto tipo_produto) {
+        this.tipo_produto = tipo_produto;
+    }
 }
